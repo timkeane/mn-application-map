04ca0d8b3750939c9b4dd16a7ca24c7f97382bf3
Tim Keane <t.d.keane@gmail.com>
2025-09-11 17:22:44 -0400

    download data

--- js/feature/bsl.js
+++ js/feature/bsl.js
@@ -20,9 +20,9 @@ export default function html(feature, type) {
   const html = $(`<div id="${id}" class="feature-html ${css}"><h4 class="focus-ring" tabindex="0"><span data-i18n="layer.bsl.name"></span></h4></div>`)
     .append(getDistance(feature))
     .append(`<address>${address}</address>`)
-    .append(`<div class="location-id"><span class="field" data-i18n="[prepend]feature.field.id">:</span> <span>${locationId}</span></div>`)
-    .append(`<div class="location-type"><span class="field" data-i18n="[prepend]feature.field.loctype">:</span> <span>${locType}</span></div>`)
-    .append(`<div class="mdu"><span class="field" data-i18n="[prepend]feature.field.mdu">:</span> <span>${mdu}</span></div>`)
+    .append(`<div class="location-id"><span class="field" data-i18n="[prepend]feature.field.id">:</span> <span>${locationId}</span></div>`);
+    // .append(`<div class="location-type"><span class="field" data-i18n="[prepend]feature.field.loctype">:</span> <span>${locType}</span></div>`)
+    // .append(`<div class="mdu"><span class="field" data-i18n="[prepend]feature.field.mdu">:</span> <span>${mdu}</span></div>`)
     // .append(speed ? `<div class="speed"><span class="field" data-i18n="[prepend]feature.field.speed">:</span> <span>${speed}</span></div>` : '')
   appendChallengeButton(feature, html);
 
